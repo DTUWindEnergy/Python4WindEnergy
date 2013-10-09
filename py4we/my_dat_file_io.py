ca9ec9f6a8c421dbb29c304866056a47b15a1fc1
Pierre-Elouan Réthoré <pire@dtu.dk>
2013-10-09 17:05:23 +0200

    FileType: implementing Mads modifications

---
+++ py4we/my_dat_file_io.py
@@ -0,0 +1,70 @@
+""" IO classes for YOUR_FILE_TYPE_NAME file types
+
+Copyright (C) 2013 DTU Wind Energy
+
+Author: YOUR_NAME
+Email: YOUR_EMAIL
+Last revision: DATE
+
+License: Apache v2.0, http://www.apache.org/licenses/LICENSE-2.0
+"""
+
+
+from __future__ import print_function
+from we_file_io import WEFileIO, TestWEFileIO
+import unittest
+
+
+
+### Your class should look like this one ---------------------------------
+
+
+class MyDatFileIO(WEFileIO):
+    """ Description of your MyFileType class should be here.
+    You can also describe the different methods.
+
+    methods:
+    --------
+        write: write a file
+        reade: read a file
+
+    """
+    def _write(self):
+        """ Write a file (overrided)
+        """
+        # HERE DO SOMETHING TO PREPARE THE DATA TO BE WRITTEN ############
+        with open(self.filename, 'w') as f:
+            f.write(self.data)
+
+
+    def _read(self):
+        """ Read the file (overrided)
+        """
+        with open(self.filename, 'r') as f:
+            self.data = f.read()
+        # HERE DO SOMETHING TO PREPARE THE DATA TO BE READ ############
+
+
+
+
+## Do Some testing -------------------------------------------------------
+class TestMyDatFileIO(TestWEFileIO):
+    """ Test class for MyFileType class """
+
+    def test_duplication(self):
+        self._test_duplication(MyDatFileIO, 'test_file.dat')
+
+
+## Main function ---------------------------------------------------------
+if __name__ == '__main__':
+    """ This is the main fuction that will run the tests automatically
+
+    $> python my_file_type.py
+    .
+    ----------------------------------------------------------------------
+    Ran X test in XXXs
+
+    OK
+    """
+    unittest.main()
+
---
+++ py4we/we_file_io.py
@@ -0,0 +1,105 @@
+""" IO classes for the DTU FileType class
+
+Copyright (C) 2013 DTU Wind Energy
+
+Authors: Pierre-Elouan Rethore, Mads Mølgaard Pedersen
+Email: pire@dtu.dk, mmpe@dtu.dk
+Last revision: 9/10/2013
+
+License: Apache v2.0, http://www.apache.org/licenses/LICENSE-2.0
+"""
+
+class WEFileIO(object):
+    """Generic IO classe for file types classes."""
+
+    def __init__(self, filename=None):
+        """ Initialized the classe using the filename
+
+        Parameters:
+        ----------
+        filename : string (optional)
+                   The file name to read and write
+        """
+        if filename:
+            ### If there is a new filename, replace the object variable
+            self.filename = filename
+            ### If the filename is provided, read the file
+            self.read()
+
+    def read(self, filename=None):
+        """ Read the file
+        Parameters:
+        ----------
+        filename : string (optional)
+                   The file name to read
+
+        Returns:
+        --------
+        data : string
+        	   the data read
+        """
+        if filename:
+            ### If there is a new filename, replace the object variable
+            self.filename = filename
+
+        if self.filename:
+            self._read()
+            return self.data
+        else:  # If self.filename == None, raise an exception
+            raise Exception('No filename has been provided')
+
+    def write(self, filename=None):
+        """ Write a file
+
+        Parameters:
+        ----------
+        filename : string (optional)
+                   The file name to write
+        """
+
+        if filename:
+            # If there is a new filename, replace the instance variable
+            self.filename = filename
+
+        if self.filename:
+            self._write()
+        else:
+            # If self.filename == None, raise an exception
+            raise Exception('No filename has been provided')
+
+    ### Private methods to be implemented in the subclasses --------------------
+    def _read(self):
+        """ Read the file."""
+        ### You are going to replace this code when you inherit from this class
+        raise NotImplementedError("This method must be implemented in subclass")
+
+    def _write(self):
+        """ Write a file"""
+        ### You are going to replace this code when you inherit from this class
+        raise NotImplementedError("This method must be implemented in subclass")
+
+
+
+## Do Some testing -------------------------------------------------------
+import unittest
+
+class TestWEFileIO(unittest.TestCase):
+    """ Test class for FileType class """
+
+    def _test_duplication(self, class_, filename):
+        """ Test if a file is written correctly by comparing with the data
+        of the original file
+        """
+        original_filename = filename
+        new_filename = 'new_' + original_filename
+
+        ### Open a new file
+        original_file = class_(original_filename)
+        ### write the file to a new filename
+        original_file.write(new_filename)
+
+        new_file = class_(new_filename)
+
+        ### Unit test function to check if two things are equal
+        self.assertEqual(original_file.data, new_file.data)
+
