c93c56bb213bd88cfddf89b9685406b8aab483c7
Emmanuel Branlard <ebra@dtu.dk>
2013-11-25 17:05:06 +0100

    oin: adding possibility to export to library calls

--- py4we/oin_file_io.py
+++ py4we/oin_file_io.py
@@ -21,11 +21,62 @@ import os.path as path
 from fortran_namelist_io  import FortranNamelistIO , TestFortranNamelist
 import tempfile
 import os
+import sys
 
 
 class OmnivorInputFile(FortranNamelistIO):
-    pass
 
+    def export_lib_command(self, filename_out=None):
+        """ Converts input file data into library commands and write it to a file
+        """
+        # Default comment character and booleans (python style)
+        CommentChar='#'
+        TrueVal='True'
+        FalseVal='False'
+        
+        # Output to stdout or file
+        if filename_out==None:
+            f=sys.stdout
+        else:
+            fileName, fileExtension = os.path.splitext(filename_out)
+            f=open(filename_out, 'w')
+            # If it's a matlab file, use matlab syntax
+            if fileExtension == '.m':
+                CommentChar='%'
+                TrueVal='true'
+                FalseVal='false'
+
+        # map between namelist and library wrapper set function
+        Name2Function={
+                'commondata':"omnivor.set_common_var",
+                'algo':      "omnivor.set_algo_var",
+                'coupling':  "%s error"%CommentChar ,
+                'wind':  "omnivor.set_wind_var",
+                'param':  "%s error"%CommentChar
+                }
+
+        # Looping and writting
+        for nml in self.data :
+                # Sorting dictionary data (in the same order as it was created, thanks to id)
+                SortedList = sorted(self.data[nml].items(), key=lambda(k, v): v['id'])
+
+                f.write('%s --------------------------------------------------------------------------------\n' % CommentChar)
+                f.write('%s --- %s \n' % (CommentChar,nml))
+                f.write('%s --------------------------------------------------------------------------------\n' % CommentChar)
+                for param in map(lambda(k,v):k,SortedList):
+                    value=','.join(self.data[nml][param]['val'] );
+                    if(value=='.true.'):
+                        # Dealing with booleans
+                        value=TrueVal
+                    elif(value=='.false.'):
+                        value=FalseVal
+                    else:
+                        # Dealing with arrays
+                        if value.count(',')>0:
+                            value='['+value+']'
+
+                    f.write('%s(\'%s\',%s)\n' % ( Name2Function[nml.lower()], param,value))
+# 
 ## Do Some testing -------------------------------------------------------
 class TestOmnivorInput(TestWEFileIO):
     """ Test class for MyFileType class """
@@ -34,6 +85,7 @@ class TestOmnivorInput(TestWEFileIO):
     #InputFile=OmnivorInputFile(test_file)
     #print(InputFile['Wind'])
     
+    InputFile.export_lib_command('./test/omnivor/freewake_nm80.m')
 
     def test_output_identical(self):
         InputFile=OmnivorInputFile(self.test_file)
