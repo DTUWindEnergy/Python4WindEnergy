59b03e35d14929bfdb80a4a5ffbd3c19d97f7500
ivah <ivah@dtu.dk>
2014-01-29 12:08:08 +0100

    added plot function to IO and modified magic_int

--- py4we/NM80PowerIO.py
+++ py4we/NM80PowerIO.py
@@ -4,7 +4,7 @@ Copyright (C) 2013 DTU Wind Energy
 
 Author: Iva Hrgovan
 Email: ivah@dtu.dk
-Last revision: 09-12-2013
+Last revision: 29-01-2014
 
 License: Apache v2.0, http://www.apache.org/licenses/LICENSE-2.0
 """
@@ -13,7 +13,7 @@ License: Apache v2.0, http://www.apache.org/licenses/LICENSE-2.0
 from __future__ import print_function
 from we_file_io import WEFileIO, TestWEFileIO
 import unittest
-
+import matplotlib.pyplot as plt
 
 
 class NM80PowerIO(WEFileIO):
@@ -29,13 +29,16 @@ class NM80PowerIO(WEFileIO):
 
 
     """
+    v = []
+    p = []
+    
     def _write(self):
         """ Write a file (overrided)
         """
         
         with open('test1.dat', 'w') as f:
             for line in self.items:
-                if line[0] != "%":
+                if line[0] != "%": # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
                     f.write(line + '\n')
 
 
@@ -47,6 +50,18 @@ class NM80PowerIO(WEFileIO):
             
         self.items = self.data.split('\n')
         
+        for i in range(3, len(self.items)):
+            self.v.append(float(self.items[i].split()[0]))
+            self.p.append(float(self.items[i].split()[1]))
+            
+        
+    def _plot(self,fig):
+        axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+        axes.plot(self.v, self.p, 'r')
+		
+        axes.set_xlabel('V [m/s]')
+        axes.set_ylabel('P [kW]')
+        axes.set_title('NM80 Power Curve');
 
 
 ### Main function ---------------------------------------------------------
@@ -63,3 +78,4 @@ if __name__ == '__main__':
 #    unittest.main()
     t = NM80PowerIO('PowerExp_NM80_3deg.dat')
     t.write()
+    t.plot()
